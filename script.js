57528537acc0e5abd050522b307f6126a53db1b9
santyillana <dsantiagosilvah@gmail.com>
2026-01-06 17:25:47 -0500

    Add filter function for vehicle display

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function filtrar(tipo) {
+  const vehiculos = document.querySelectorAll('.vehiculo');
 
+  vehiculos.forEach(v => {
+    if (tipo === 'todos' || v.dataset.tipo === tipo) {
+      v.style.display = 'block';
+    } else {
+      v.style.display = 'none';
+    }
+  });
+}
